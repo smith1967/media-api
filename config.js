7c275e32e676fd0429ea24465165dcad1d6e1e45
Smith Sookkhi <smith.sookkhi@gmail.com>
2019-06-10 14:43:13 +0700

    add config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    options: {
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'admin$cstc',
+            database: 'teacher_media',
+            charset: 'utf8'
+        }
+    }
+}
